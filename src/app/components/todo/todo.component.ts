0b461a07d79de193b67cd21bba1e1304af615d94
Jonathan Manzi Mihigo <la217659@student.helha.be>
2024-02-12 09:53:21 +0100

    Added comments

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -32,15 +32,20 @@ export class TodoComponent implements OnInit{
     this.refresh();
   }
 
+  /**
+   * On add a new to-do
+   */
   onSave(){
-
+    //Task added by a user
     this.newToDo.taskText = this.toDoText;
+    // By default, the task isn't finished so is set to false
     this.newToDo.taskStatus = false;
 
+    // Sending the task in the backend
     this.toDoService.addToDo(this.newToDo).subscribe({
       next: value => {
-        this.toDoText = "";
-        this.refresh();
+        this.toDoText = ""; // Clearing the variable
+        this.refresh(); // Refreshing the component
       },
       error: err => {
         console.log(err);
@@ -49,18 +54,29 @@ export class TodoComponent implements OnInit{
     });
   }
 
+  /**
+   * On task completed
+   * @param id of the task
+   */
   onFinished(id: number){
     this.taskToUpdate = this.toDoList.find(value => value.id === id)!;
-    this.taskToUpdate.taskStatus = true;
+    this.taskToUpdate.taskStatus = true; // Task completed
 
+    //Updating in the backend
     this.toDoService.updateToDo(id, this.taskToUpdate).subscribe();
   }
 
+  /**
+   * On show a list of all todos
+   */
   onShowTasks(){
     this.showTasks = !this.showTasks;
   }
 
-
+  /**
+   * On delete a specific task
+   * @param id of the task
+   */
   onDelete(id: number){
     console.log("Delete "+ id)
     this.toDoService.deleteToDo(id).subscribe({
@@ -70,6 +86,9 @@ export class TodoComponent implements OnInit{
 
   }
 
+  /**
+   * Refresh the to-do list
+   */
   refresh(){
     this.toDoService.getToDos().subscribe({
       next:(value:ToDo[]) => {
--- src/app/service/to-do.service.ts
+++ src/app/service/to-do.service.ts
@@ -10,19 +10,37 @@ export class ToDoService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * In CRUD operations, this is the C part, Create
+   * @param task
+   */
   addToDo(task:NewToDo){
     return this.http.post<ToDo[]>(`${environment.apiRoot}` + "/api/tasks/", task);
   }
 
+  /**
+   *In CRUD operations, this is the R part, Read
+   */
   getToDos(){
     return this.http.get<ToDo[]>(`${environment.apiRoot}` + "/api/tasks/");
   }
 
+  /**
+   * In CRUD operations, this is the U part, Update
+   *
+   * @param id of the task to update
+   * @param task task updated but not sent yet
+   */
   updateToDo(id: number, task: ToDo){
     return this.http.put(`${environment.apiRoot}` + "/api/tasks/"+id+"/", task );
 
   }
 
+  /**
+   * In CRUD operations, this is the D part, Delete
+   *
+   * @param id
+   */
   deleteToDo(id: number){
     return this.http.delete(`${environment.apiRoot}` + "/api/tasks/"+id+"/");
 
